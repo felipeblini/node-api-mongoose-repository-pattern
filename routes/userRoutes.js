f17a29384141af27029928559dfd9d28544b348f
Felipe blini <Felipe.blini@gmail.com>
2016-12-11 02:40:27 -0300

    Removed console.log from route file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,6 @@ const userCtrl = require('../controllers/userCtrl');
 
 const ctrl = new userCtrl();
 
-console.log(ctrl);
-
-console.log(ctrl.repo);
-
 module.exports = function(app) {
     app.get('/users', ctrl.doListUsers);
     app.post('/users/create', ctrl.doCreateUser);
